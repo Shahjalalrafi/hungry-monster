498e944eba6631846f465bae8969ae9c36fc6808
shahjalal rafi <shahjalalrafi5@gmail.com>
2021-02-07 20:35:49 +0300

    upload

--- index.js
+++ index.js
@@ -1,69 +1,81 @@
-'use strict';
-let allItems = []
-const search = document.getElementById('btn');
+const allItems = document.getElementById('foods');
+const searchBtn = document.getElementById('btn');
+const error = document.getElementById('error');
+
+//SEARCHING ITEM
+searchBtn.addEventListener('click', function () {
+    const inputValue = document.getElementById('input').value;
+    allItems.innerHTML = '';
+    if (inputValue === '') {
+        error.style.display = 'block';
+    } else {
+        getFood(inputValue);
+        error.style.display = 'none';
+    }
+});
 
 
-
-fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood')
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data);
-        allItems.push(data)
-        displayItems(data.meals)
-    });
-
-
-const displayItems = (items) => {
-    const itemsDiv = document.getElementById('all-recipies');
-
-    items.forEach((item) => {
-        const itemDiv = document.createElement('div');
-        itemDiv.className = 'col-md-3';
-        const itemInfo = `
-                    <div onclick="displayDetails('${item.idMeal}')" class="border rounded text-center h-100" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                        <div class="card-body">
-                            <img class="card-img img-fluid rounded" src="${item.strMealThumb}" alt="">
-                            <h4 class="card-title h5">${item.strMeal}</h4>
-                        </div>
-                    </div>
-                `;
-        itemDiv.innerHTML = itemInfo;
-        itemsDiv.appendChild(itemDiv);
-    });
-};
-
-const displayDetails = (name) => {
+// ITEM DETAILS
+const displayDetails = name => {
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${name}`;
     fetch(url)
-        .then((res) => res.json())
-        .then((data) => {
-            renderItemsInfo(data.meals[0]);
+        .then(res => res.json())
+        .then(data => {
+            renderFoodInfo(data.meals[0]);
             console.log(data.meals[0]);
         });
 };
 
-const renderItemsInfo = (item) => {
-    const itemsDetailsDiv = document.getElementById('itemDetails');
-
-    itemsDetailsDiv.innerHTML = `
-        <img class="img-fluid rounded mb-4" src="${item.strMealThumb}" alt="">
-        <h4>${item.strMeal}</h4>
-        
-        <h5 class="pt-3 pb-2"><i class="icon-fire icons"></i> Ingredients</h5>
-        <ul class="list-unstyled mb-0">
-            <li><i class="icon-check icons"></i> ${item.strIngredient1}</li>
-            <li><i class="icon-check icons"></i> ${item.strIngredient2}</li>
-            <li><i class="icon-check icons"></i> ${item.strIngredient3}</li>
-            <li><i class="icon-check icons"></i> ${item.strIngredient4}</li>
-            <li><i class="icon-check icons"></i> ${item.strIngredient5}</li>
-        </ul>
-    `;
+// RENDER ITEM
+const renderFoodInfo = (food) => {
+    const ingredients = [];
+    for (let i = 1; i <= 20; i++) {
+        if (food[`strIngredient${i}`]) {
+            ingredients.push(`${food[`strIngredient${i}`]} - ${food[`strMeasure${i}`]}`);
+        } else {
+            // Stop if there are no more ingredients
+            break;
+        }
+    }
+    const foodDetailsDiv = document.getElementById('foodsDetails');
+    foodDetailsDiv.innerHTML = `
+    <img class="img-fluid rounded mb-4" src="${food.strMealThumb}" alt="">
+    <h4>${food.strMeal}</h4>
+    
+    <h5 class="pt-3 pb-2"><i class="icon-fire icons"></i> Ingredients</h5>
+    <ul class="list-unstyled mb-0">
+    ${ingredients.map((ingredient) => `<li><i class="icon-check icons"></i>${ingredient}</li>`).join('')}
+    </ul>
+`;
 };
 
-console.log(allItems)
+// items
+function getFood(mealId) {
+    const apiLink = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealId}`;
 
+    fetch(apiLink)
+        .then(res => res.json())
+        .then(data => {
+            displayFoods(data.meals);
+        });
 
-
-
-
-
+    const displayFoods = foods => {
+        const foodsDiv = document.getElementById('foods');
+        if (foods != null) {
+            foods.map(food => {
+                const itemDiv = document.createElement('div');
+                itemDiv.className = 'col-md-3';
+                const itemInfo = `
+                        <div onclick="displayDetails('${food.idMeal}')" class="border rounded text-center h-100" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        <img class="img-fluid rounded-top" src="${food.strMealThumb}" alt="">
+                        <h4 class="h5 py-4 px-2 mb-0">${food.strMeal}</h4>
+                        </div>
+                    `;
+                itemDiv.innerHTML = itemInfo;
+                foodsDiv.appendChild(itemDiv);
+            });
+        } else {
+            error.style.display = 'block';
+        }
+    };
+}
